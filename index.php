60a8e9384cefc2329115410999d3916ce63d06c2
raghav1674 <45117311+raghav1674@users.noreply.github.com>
2020-07-10 03:57:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,33 @@
 <?php
 
-class A{
 
-public $name;
 
-function __construct($name){
 
-	$this->name=$name;
-}
 
-}
 
-$obj=new A("Hello from PHP");
+echo("hello from php");
 
-echo $obj->name;
+
+
+
 
 
 ?>
+
+// class A{
+
+// public $name;
+
+// function __construct($name){
+
+// 	$this->name=$name;
+// }
+
+// }
+
+// $obj=new A("Hello from PHP");
+
+// echo $obj->name;
+
+
+// ?>
